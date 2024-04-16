5ebceabafd5f3cabb921f8606d762a33cb2527a8
Santiago0709 <santi@alphadev.io>
2024-04-16 17:17:58 -0600

    Sales Crud

--- src/controllers/SalesController.js
+++ src/controllers/SalesController.js
@@ -11,17 +11,58 @@ const save = async (req, res) => {
       data: sale,
     });
 
-    return res.status(201).send({new_sale,message:"Sale created"})
-    
+    return res.status(201).send({ new_sale, message: "Sale created" })
+
   } catch (error) {
     console.log(error);
     if (error.code === 'P2002' && error.meta.target === 'id_lot') {
 
-        return res.status(400).json({ error: "Duplicate entry for lot ID detected" });
+      return res.status(400).json({ error: "Duplicate entry for lot ID detected" });
     }
 
-    return res.status(500).send({error:error.message})
+    return res.status(500).send({ error: error.message })
   }
 };
 
-module.exports.SalesController = {save}
+const fetch = async (req, res) => {
+  try {
+    const result = await prisma.sales.findMany({
+      include:{
+        lots: true,
+        clients: true,
+      }
+    })
+
+    return res.status(200).send({ result })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    let payload = req.body
+    const sale = new Sale(payload)
+    let data = {}
+
+    if(sale.price !== undefined || sale.price !== null){
+      data.price = sale.price
+    }
+
+    const updatedSale = await prisma.sales.update ({
+      data:data,
+      where:{
+        id: sale.id
+      }
+    })
+
+    return res.status(200).send({ message: "Venta modificada exitosamente", updatedSale });
+
+  } catch (error) {
+    return res.status(500).send({error: error.message});
+  }
+}
+
+
+
+module.exports.SalesController = { save, fetch, update }
--- src/routes/SalesRoutes.js
+++ src/routes/SalesRoutes.js
@@ -4,5 +4,7 @@ const { SalesController } = require("../controllers/SalesController");
 const SalesRoutes = express.Router()
 
 SalesRoutes.post('/',SalesController.save)
+SalesRoutes.get('/',SalesController.fetch)
+SalesRoutes.put('/',SalesController.update)
 
 module.exports.SalesRoutes = SalesRoutes
