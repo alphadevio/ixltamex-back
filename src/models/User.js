c6bf9124fd26b5fd9ef3ed0cc17155f375fa8af4
stevemen20020 <david@alphadev.io>
2024-05-17 09:10:22 -0600

    updated user model to allow edit without pass

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,8 @@
 class User {
     constructor(payload){
-        if (!payload.name || !payload.email || !payload.phone_number || !payload.password || !payload.profile_id) {
-            throw new Error('Missing required parameters');
-        }
+        // if (!payload.name || !payload.email || !payload.phone_number || !payload.password || !payload.profile_id) {
+        //     throw new Error('Missing required parameters');
+        // }
         this.id = payload.id
         this.name = payload.name
         this.email = payload.email
