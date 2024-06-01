6cbaaf5c0199796661db19009502b70ba1382c25
stevemen20020 <david@alphadev.io>
2024-06-01 10:05:41 -0600

    Updated usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -25,7 +25,7 @@ const save = async (req, res) => {
     });
     
     if (repeated_email) {
-      throw new Error("Existe una cuenta con ese correo");
+      throw new Error("Existe una cuenta con ese usuario");
     }
     const new_user = await prisma.users.create({
       data: user
