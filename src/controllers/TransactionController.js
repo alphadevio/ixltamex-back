d024acd675476a4c8b66ce3dbdc47c5617187102
stevemen20020 <david@alphadev.io>
2024-07-10 14:58:01 -0600

    UTC

--- src/controllers/TransactionController.js
+++ src/controllers/TransactionController.js
@@ -73,9 +73,9 @@ const fetch = async (req, res) => {
     }
 
     if (date) {
-        const fecha = new Date(date)
+        const fecha = new Date.UTC(date)
         fecha.setDate(fecha.getDate() + 1)
-        baseQuery = baseQuery.where('transactions.created_at', '>=', new Date(date));
+        baseQuery = baseQuery.where('transactions.created_at', '>=', new Date.UTC(date));
         baseQuery = baseQuery.where('transactions.created_at', '<=', fecha);
     }
 
